e695b2afe37ba2ea2bec20fa4d9571026f9ba0fb
unknown <2726012413@qq.com>
2020-05-09 17:02:27 +0800

    Initial commit

---
+++ analysis/data/label_analysis.py
@@ -0,0 +1,49 @@
+import os
+import xml.etree.ElementTree as ET
+from analysis.visualization.pie import Pie
+
+
+def label_analysis(root_path, classes=None):
+    """
+    :param root_path:
+    :param classes:
+    :return:
+    """
+    # initialization
+    path_queue = [root_path]
+    img_quantity = 0
+    label_quantity_per_class = {}
+    label_quantity_per_image = []
+
+    # traverse root path
+    while path_queue:
+        current_path = path_queue.pop(0)
+        for item in os.listdir(current_path):
+            item = os.path.join(current_path, item)
+            if os.path.isdir(item):
+                path_queue.append(item)
+            # quantity of each annotation class
+            elif item.split('.')[-1].lower() == "xml":
+                with open(item, encoding="utf-8") as file:
+                    label_quantity_image = 0
+                    tree = ET.parse(file)
+                    tree_root = tree.getroot()
+                    for obj in tree_root.iter('object'):
+                        cls = obj.find("name").text
+                        if classes is None or cls in classes:
+                            label_quantity_per_class.setdefault(cls, 0)
+                            label_quantity_per_class[cls] += 1
+                            label_quantity_image += 1
+                    label_quantity_per_image.append(label_quantity_image)
+            # total image number
+            elif item.split('.')[-1].lower() in ["jpg", "jpeg", "png"]:
+                img_quantity += 1
+    print(img_quantity)
+    pie_graph = Pie()
+    pie_graph.pic_for_dict(label_quantity_per_class)
+    for item in sorted(label_quantity_per_class.items(), key=lambda k: k[1], reverse = True):
+        print("{}:{}".format(item[0], item[1]))
+
+
+if __name__=="__main__":
+    label_analysis("D:/shudian")
---
+++ analysis/visualization/pie.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Pie:
+    def __init__(self, figsize=(8, 6)):
+        self.figsize = figsize
+
+    def autopct_func(self, pct, allvals):
+        # absolute = int(pct / 100. * np.sum(allvals))
+        return "{:.1f}%".format(pct)
+
+    def pic_for_dict(self, data):
+        data = sorted(data.items(), key=lambda k: k[1], reverse = True)
+        vals = [item[1] for item in data]
+        ingredients = ["{:<10d}:{}".format(item[1], item[0]) for item in data]
+
+        fig, ax = plt.subplots(figsize=self.figsize, subplot_kw=dict(aspect="equal"))
+        wedges, texts, autotexts = ax.pie(vals, autopct=lambda pct: self.autopct_func(pct, vals),
+                                          textprops=dict(color="w"))
+        ax.legend(wedges, ingredients,
+                  title="Ingredients",
+                  loc="center left",
+                  bbox_to_anchor=(1, 0, 0.5, 1))
+        plt.setp(autotexts, size=6, weight="light")
+        plt.show()
