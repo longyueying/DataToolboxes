9a3c30e53a38a5ae0c015df73ad063776fd540d0
unknown <2726012413@qq.com>
2020-06-08 17:30:05 +0800

    xml mapping

--- utils/xml_util.py
+++ utils/xml_util.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 import os
 
 
-def elementTree_generator(root_path):
+def xml_path_generator(root_path):
     """
     :param root_path:
     :return: prefix of xml file and elementTree root
@@ -12,13 +12,26 @@ def elementTree_generator(root_path):
     while path_queue:
         current_path = path_queue.pop(0)
         for item in os.listdir(current_path):
+            current_name = item
             item = os.path.join(current_path, item)
             if os.path.isdir(item):
                 path_queue.append(item)
-            # quantity of each annotation class
             elif item.split('.')[-1].lower() == "xml":
-                xml_prefix = item.split('\\')[-1].split('/')[-1].split('.')[0]
-                with open(item, encoding="utf-8") as file:
-                    tree = ET.parse(file)
-                    tree_root = tree.getroot()
-            yield xml_prefix, tree_root
+                yield current_path, current_name
+
+
+def classes_mapping(root_path, mapping_dict, signature):
+    for current_path, xml_name in xml_path_generator(root_path):
+        target_path = current_path + '_' + signature
+        if not os.path.exists(target_path):
+            os.makedirs(target_path)
+        else:
+            source_file = os.path.join(current_path, xml_name)
+            target_path = os.path.join(target_path, xml_name)
+            with open(source_file, "r", encoding="utf-8") as f:
+                pass
+
+
+
+if __name__ == "__main__":
+    classes_mapping("E:/Data/biandian/2019-biandian-1936/Annotations", {}, "e")
