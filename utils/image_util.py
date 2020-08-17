06d7e00187d657ed1507cea509cc12c50e3d5475
unknown <2726012413@qq.com>
2020-08-17 10:09:16 +0800

    image rename function

--- utils/image_util.py
+++ utils/image_util.py
@@ -1,4 +1,6 @@
 import os
+from tqdm import tqdm
+from analysis.data.image_analysis import get_image_number
 
 
 def image_path_generator(root_path_list):
@@ -20,8 +22,14 @@ def image_path_generator(root_path_list):
 
 
 def image_rename(root_path_list):
-    for path, name in image_path_generator(root_path_list):
+    length = get_image_number(root_path_list)
+    rename_file_number = 0
+
+    for path, name in tqdm(image_path_generator(root_path_list), total=length):
         if '.JPG' in name or '.JPEG' in name or '.jpeg' in name:
+            rename_file_number += 1
             file_path = os.path.join(path, name)
             file_path_rename = file_path.replace(".JPG", ".jpg").replace('.JPEG', '.jpg').replace('.jpeg', '.jpg')
             os.rename(file_path, file_path_rename)
+
+    print("There are {}/{} images have be renamed".format(rename_file_number, length))
